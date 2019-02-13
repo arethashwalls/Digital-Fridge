169cf57ee7eec48e1070a76e30e147f522a97eb3
bking1989 <bradley_king@me.com>
2019-02-13 01:08:18 -0700

    Got new user modal and login button to work

---
+++ public/js/splash.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  // New User Modal
+  $("#newUserBtn").on("click", function() {
+    $("#newUserModal").modal("show");
+  });
+
+  // New User Submit
+  $("#newSubmitBtn").on("click", function() {
+    var newUser = $("input#newUserName").val().trim();
+    $("#dropdownMenu").append(`<option value="${newUser}">${newUser}</option>`);
+    console.log("New user submitted!");
+    $("input#newUserName").empty();
+  });
+
+  // Username Select
+  $("#loginBtn").on("click", function() {
+    var userSelect = $("#dropdownMenu").val().trim();
+    console.log(userSelect);
+  });
+});
