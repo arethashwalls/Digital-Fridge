692dcb8ee241ccf2f4fdb0e893cd6d823e1e0202
Blue Buffalo <anettagoldsher@gmail.com>
2019-02-13 21:41:29 -0700

    merged with master

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,17 +2,11 @@ var db = require("../models");
 
 module.exports = function (app) {
   // Get all examples
-<<<<<<< HEAD
-  app.get("/api/examples", function (req, res) {
-    db.Example.findAll({}).then(function (dbExamples) {
-      res.json(dbExamples);
-=======
-  app.get("/api/users", function(req, res) {
+  app.get("/api/users", function (req, res) {
     db.User.findAll({
       attributes: ["username"]
-    }).then(function(data) {
+    }).then(function (data) {
       res.json(data);
->>>>>>> master
     });
     // db.Ingredient.findAll({
     //   include: [db.User]
@@ -21,26 +15,6 @@ module.exports = function (app) {
     // });
   });
 
-<<<<<<< HEAD
-  // Create a new example
-  app.post("/api/examples", function (req, res) {
-    db.Example.create(req.body).then(function (dbExample) {
-      res.json(dbExample);
-    });
-  });
-
-  // Delete an example by id
-  app.delete("/api/examples/:id", function (req, res) {
-    db.Example.destroy({
-      where: {
-        id: req.params.id
-      }
-    }).then(function (dbExample) {
-      res.json(dbExample);
-    });
-  });
-};
-=======
   // // Create a new example
   // app.post("/api/examples", function(req, res) {
   //   db.Example.create(req.body).then(function(dbExample) {
@@ -55,4 +29,3 @@ module.exports = function (app) {
   //   });
   // });
 };
->>>>>>> master
