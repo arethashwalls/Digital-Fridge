7202f1bbc2159bcc08449127372c329ace832036
jley914 <jleyva914@gmail.com>
2019-02-16 11:00:09 -0700

    inventoryI html file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,30 +2,37 @@ var db = require("../models");
 
 module.exports = function (app) {
   // Get all examples
-  // app.post("/api/examples", function (req, res) {
-  //   db.Example.create(req.body).then(function (dbExample) {
-  //     res.json(dbExample);
-  //   });
-  // });
-  app.get("/api/users", function (req, res) {
+  app.post("/api/examples", function (req, res) {
+    db.Example.create(req.body).then(function (dbExample) {
+      res.json(dbExample);
+    });
+  });
+  app.get("/api/use", function (req, res) {
     db.User.findAll({
       attributes: ["username"]
     }).then(function (data) {
       res.json(data);
     });
   });
+  app.get("/api/inventoryI", function (req, res) {
+    db.User.findAll({
+      attributes: ["user"]
+    }).then(function (data) {
+      res.json(data);
+    });
+  });
 
-  // // Create a new example
-  // app.post("/api/examples", function(req, res) {
-  //   db.Example.create(req.body).then(function(dbExample) {
-  //     res.json(dbExample);
-  //   });
-  // });
+  // Create a new example
+  app.post("/api/examples", function (req, res) {
+    db.ingredient.create(req.body).then(function (dbExample) {
+      res.json(dbExample);
+    });
+  });
 
-  // // Delete an example by id
-  // app.delete("/api/examples/:id", function(req, res) {
-  //   db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-  //     res.json(dbExample);
-  //   });
-  // });
+  // Delete an example by id
+  app.delete("/api/examples/:id", function (req, res) {
+    db.Example.destroy({ where: { id: req.params.id } }).then(function (dbExample) {
+      res.json(dbExample);
+    });
+  });
 };
