b8b8fb0a629b0a84213be06f12be8ee9bf9506fb
jley914 <jleyva914@gmail.com>
2019-02-19 18:35:08 -0700

    inventory

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -21,7 +21,8 @@ module.exports = function(app) {
   app.get("/api/:userid/ingredients", function(req, res) {
     db.Ingredient.findAll({
       where: {
-        userId: req.params.userid
+        userId: req.params.userid,
+        quantityOwned: { [Op.gt]: 0 }
       },
       include: [db.User]
     }).then(function(data) {
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,38 +1,56 @@
 var db = require("../models");
+var sequelize = require("sequelize");
+var Op = sequelize.Op;
 
-module.exports = function(app) {
-  app.get("/", function(req, res) {
-    db.User.findAll({ attribute: ["username"] }).then(function(data) {
+module.exports = function (app) {
+  app.get("/", function (req, res) {
+    db.User.findAll({ attribute: ["username"] }).then(function (data) {
       var obj = {
         usernames: data
       };
 
-      res.render("login", obj);
+      //     res.render("login", obj);
+      //   });
+      // });
+      // Load index page
+      // app.get("/", function(req, res) {
+      //   db.Example.findAll({}).then(function(dbExamples) {
+      //     res.render("index", {
+      //       msg: "Welcome!",
+      //       examples: dbExamples
+      //     });
+      //   });
+      // });
+
+      // // Load example page and pass in an example by id
+      // app.get("/example/:id", function(req, res) {
+      //   db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+      //     res.render("example", {
+      //       example: dbExample
+      //     });
+      //   });
+      // });
+      // app.get("/:userid/ingredients/shoppinglist", function(req, res) {
+      //   res.render("shoping");
+      // });
+      app.get("/:userid/ingredients", function(req, res) {
+        db.Ingredient.findAll({
+          where: {
+            userId: req.params.userid
+          },
+          include: [db.User]
+        }).then(function(data) {
+          var obj = {
+            ingredient: data
+          };
+          res.render("inventory", obj);
+        });
+      });
+
+      // // Render 404 page for any unmatched routes
+      // app.get("*", function(req, res) {
+      //   res.render("404");
+      // });
     });
   });
-  // Load index page
-  // app.get("/", function(req, res) {
-  //   db.Example.findAll({}).then(function(dbExamples) {
-  //     res.render("index", {
-  //       msg: "Welcome!",
-  //       examples: dbExamples
-  //     });
-  //   });
-  // });
-
-  // // Load example page and pass in an example by id
-  // app.get("/example/:id", function(req, res) {
-  //   db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-  //     res.render("example", {
-  //       example: dbExample
-  //     });
-  //   });
-  // });
-  app.get("/", function(req, res) {
-    res.render("shoping");
-  });
-  // // Render 404 page for any unmatched routes
-  // app.get("*", function(req, res) {
-  //   res.render("404");
-  // });
 };
