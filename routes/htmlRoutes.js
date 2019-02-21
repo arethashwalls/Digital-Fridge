fb3678ffce9d841311a7f639db79e3337dd5b4e6
jley914 <jleyva914@gmail.com>
2019-02-20 18:53:42 -0700

    inventoryfile

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,4 +1,6 @@
 var db = require("../models");
+var Sequelize = require("sequelize");
+var Op = Sequelize.Op;
 
 module.exports = function(app) {
   app.get("/", function(req, res) {
@@ -28,8 +30,21 @@ module.exports = function(app) {
   //     });
   //   });
   // });
-  app.get("/", function(req, res) {
-    res.render("shoping");
+  app.get("/:userid/ingredients", function(req, res) {
+    db.Ingredient.findAll({
+      where: {
+        userId: req.params.userid,
+        quantityOwned: {
+          [Op.gt]: 0
+        }
+      },
+      include: [db.User]
+    }).then(function(data) {
+      var obj = {
+        ingredient: data
+      };
+      res.render("inventory", obj);
+    });
   });
   // // Render 404 page for any unmatched routes
   // app.get("*", function(req, res) {
