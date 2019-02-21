b258eacbaae357c43a95866966391c2deb75d57b
bking1989 <bradley_king@me.com>
2019-02-20 18:40:38 -0700

    Got the new item button made and expanding. Need to add form.

--- public/js/shopping.js
+++ public/js/shopping.js
@@ -1,17 +1,23 @@
 $(document).ready(function() {
-$("#logOutBtn").on("click", function() {
-    window.location.href = "../../../";
-})
+    $("#logOutBtn").on("click", function() {
+        window.location.href = "../../../";
+    })
 
-$(".shoppingCheck").on("click", function() {
-    var parent = $(this).parent().parent().data("id");
+    $(".shoppingCheck").on("click", function() {
+        var parent = $(this).parent().parent().data("id");
 
-    $(`tr[data-id="${parent}"]`).children().toggleClass("bg-light");
-    $(`tr[data-id="${parent}"]`).children().toggleClass("text-muted");
-    $(`tr[data-id="${parent}"]`).children().toggleClass("checked");
-})
+        $(`tr[data-id="${parent}"]`).children().toggleClass("bg-light");
+        $(`tr[data-id="${parent}"]`).children().toggleClass("text-muted");
+        $(`tr[data-id="${parent}"]`).children().toggleClass("checked");
+    })
 
-$("#goToInventory").on("click", function() {
-    window.location.href = `../ingredients`;
-})
+    $("#goToInventory").on("click", function() {
+        window.location.href = `../ingredients`;
+    })
+
+    $("#addItemLink").on("click", function() {
+        $("#addItemLink").css("display", "none");
+        $("#addItem").toggleClass("expand", 250);
+        $("addItem").delay(250).toggleClass("container")
+    });
 });
