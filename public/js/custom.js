8d51f4b8f5a895bb2a0e5c5a96ae6a14f49c188d
jley914 <jleyva914@gmail.com>
2019-02-20 20:27:06 -0700

    updated buttons

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,8 +1,8 @@
-$(document).ready(function() {
-  $("#logOutBtn").on("click", function() {
-    window.location.href = "../../../";
+$(document).ready(function () {
+  $("#logOutBtn").on("click", function () {
+   window.location.href = "../../../";
   });
-  $("#goToShopping").on("click", function() {
-    window.location.href = "./shoppinglist";
+  $("#goToShoppinglist").on("click", function () {
+   window.location.href = "./shoppinglist";
   });
-});
+ });
