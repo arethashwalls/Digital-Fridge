e0fda676b66d01d756f687ef802d77a21dc8174a
ASH Walls <admin@arethawalls.com>
2019-02-20 20:07:47 -0700

    Got rid of forced sync and some junk files, cleaned up merge artifacts

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,15 +1,8 @@
-$(document).ready(function () {
-  $("#logOutBtn").on("click", function () {
+$(document).ready(function() {
+  $("#logOutBtn").on("click", function() {
     window.location.href = "../../../";
   });
-<<<<<<< HEAD
-  $("#goToShoppinglist").on("click", function () {
-    window.location.href = "../shoppinglist";
-  });
-});
-=======
-  $("#goToShopping").on("click", function () {
+  $("#goToShopping").on("click", function() {
     window.location.href = "./shoppinglist";
   });
 });
->>>>>>> master
